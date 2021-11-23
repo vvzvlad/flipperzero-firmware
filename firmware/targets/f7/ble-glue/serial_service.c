198087c0da31befa3d53828cf9bddf2da4cac338
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-11-23 16:30:48 +0300

    Revert log level

--- firmware/targets/f7/ble-glue/serial_service.c
+++ firmware/targets/f7/ble-glue/serial_service.c
@@ -39,7 +39,7 @@ static SVCCTL_EvtAckStatus_t serial_svc_event_handler(void *event) {
                 ret = SVCCTL_EvtAckFlowEnable;
                 FURI_LOG_D(TAG, "RX descriptor event");
             } else if(attribute_modified->Attr_Handle == serial_svc->rx_char_handle + 1) {
-                FURI_LOG_I(TAG, "Received %d bytes", attribute_modified->Attr_Data_Length);
+                FURI_LOG_D(TAG, "Received %d bytes", attribute_modified->Attr_Data_Length);
                 if(serial_svc->on_received_cb) {
                     furi_check(osMutexAcquire(serial_svc->buff_size_mtx, osWaitForever) == osOK);
                     if(attribute_modified->Attr_Data_Length > serial_svc->bytes_ready_to_receive) {
