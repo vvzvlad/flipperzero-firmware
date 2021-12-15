417e53aeeeca84065eea6807d1c585593f8ae356
vvzvlad <vvzvlad@gmail.com>
2021-12-16 01:20:27 +0300

    refactoring

--- applications/counter/counter.c
+++ applications/counter/counter.c
@@ -2,16 +2,31 @@
 #include <furi-hal.h>
 #include <gui/gui.h>
 
+typedef struct {
+    Gui* gui;
+    ViewPort* view_port;
+} CounterApp;
+
+void counter_app_free(CounterApp* app) {
+    gui_remove_view_port(app->gui, app->view_port);
+    view_port_free(app->view_port);
+    furi_record_close("gui");
+    free(app);
+}
+
+CounterApp* counter_app_alloc() {
+    CounterApp* app = furi_alloc(sizeof(CounterApp));
+    app->view_port = view_port_alloc();
+    app->gui = furi_record_open("gui");
+    gui_add_view_port(app->gui, app->view_port, GuiLayerFullscreen);
+    return app;
+}
 
 int32_t counter_app(void* p) {
-    ViewPort* view_port = view_port_alloc();
-    Gui* gui = furi_record_open("gui");
-    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+    CounterApp* app = counter_app_alloc();
 
     delay(2000);
 
-    gui_remove_view_port(gui, view_port);
-    furi_record_close("gui");
-    view_port_free(view_port);
+    counter_app_free(app);
     return 0;
 }
