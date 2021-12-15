b46be4635d51c11962333d44f6ff71c6f396b915
vvzvlad <vvzvlad@gmail.com>
2021-12-16 00:37:41 +0300

    The application that runs

--- applications/counter/counter.c
+++ applications/counter/counter.c
@@ -1,5 +1,17 @@
 #include <furi.h>
+#include <furi-hal.h>
+#include <gui/gui.h>
+
 
 int32_t counter_app(void* p) {
+    ViewPort* view_port = view_port_alloc();
+    Gui* gui = furi_record_open("gui");
+    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+
+    delay(2000);
+
+    gui_remove_view_port(gui, view_port);
+    furi_record_close("gui");
+    view_port_free(view_port);
     return 0;
 }
