a89df7b3d842c2182fc2b33c7e21ca66fa252769
vvzvlad <vvzvlad@gmail.com>
2021-12-18 13:44:21 +0300

    add exit app func on return button

--- applications/counter/counter.c
+++ applications/counter/counter.c
@@ -5,7 +5,13 @@
 typedef struct {
     Gui* gui;
     ViewPort* view_port;
+    osMessageQueueId_t event_queue;
 } CounterApp;
+
+void counter_input_callback(InputEvent* input_event, void* ctx) {
+    furi_assert(ctx);
+    osMessageQueuePut((osMessageQueueId_t)ctx, input_event, 0, 0);
+}
 
 void counter_draw_callback(Canvas* canvas, void* ctx) {
     canvas_set_font(canvas, FontPrimary);
@@ -15,6 +21,7 @@ void counter_draw_callback(Canvas* canvas, void* ctx) {
 void counter_app_free(CounterApp* app) {
     gui_remove_view_port(app->gui, app->view_port);
     view_port_free(app->view_port);
+    osMessageQueueDelete(app->event_queue);
     furi_record_close("gui");
     free(app);
 }
@@ -23,15 +30,24 @@ CounterApp* counter_app_alloc() {
     CounterApp* app = furi_alloc(sizeof(CounterApp));
     app->view_port = view_port_alloc();
     app->gui = furi_record_open("gui");
+    app->event_queue = osMessageQueueNew(8, sizeof(InputEvent), NULL);
     gui_add_view_port(app->gui, app->view_port, GuiLayerFullscreen);
     view_port_draw_callback_set(app->view_port, counter_draw_callback, app);
+    view_port_input_callback_set(app->view_port, counter_input_callback, app->event_queue);
     return app;
 }
 
 int32_t counter_app(void* p) {
     CounterApp* app = counter_app_alloc();
 
-    delay(2000);
+    while(1) {
+        InputEvent input;
+        osStatus_t result = osMessageQueueGet(app->event_queue, &input, NULL, osWaitForever);
+        furi_check(result == osOK);
+        if(input.type == InputTypeShort && input.key == InputKeyBack) {
+            break;
+        }
+    }
 
     counter_app_free(app);
     return 0;
